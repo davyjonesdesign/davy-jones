fc309f77e4ff0b3bdaede3a3de0ea6d7b1a8d880
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2026-05-04 22:47:45 -0500

    Add build-time prerender pipeline with react-snap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+const app = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app);
+} else {
+  createRoot(rootElement).render(app);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
